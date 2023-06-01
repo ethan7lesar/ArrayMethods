67a6aa12184d552a95b6e09e0254eb4fe272196c
ethan7lesar <ethanlesar11@gmail.com>
2023-06-01 15:00:49 +0200

    hoyyaa

---
+++ script.js
@@ -0,0 +1,41 @@
+let array1 = [];
+
+function add() {
+  // value = document.getElementById("input").innerHTML;
+  // array1.push(document.getElementById("input").value);
+  // console.log(array1);
+
+  // document.write("Theis is the array ", array1.values)
+  let input = document.getElementById("input").value;
+  array1.push(input);
+  //     document.write("Theis isthe array ", array1)
+  document.getElementById("display").innerHTML = array1;
+
+  // document.write("This is the array " , array1 )
+}
+
+let array2 = [];
+
+function addObject() {
+  let Value = {
+    name: document.getElementById("name").value,
+    surname: document.getElementById("surname").value,
+  };
+
+  array2.push(Value);
+
+//   document.getElementById("display1").innerHTML += Object.values(Value);
+
+  console.log(array2);
+
+  let table = document.querySelector(".tableInfo");
+  table.innerHTML = "";
+  array2.forEach((data) => {
+    table.innerHTML += ` <tr>
+    <td>${data.name}</td>
+    <td>${data.surname}</td> 
+    </tr>`;
+  
+});
+
+}
